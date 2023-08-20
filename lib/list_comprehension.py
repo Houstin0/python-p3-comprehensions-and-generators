2ad2bd8cc48b7b7bb05c581627bc8b5b3e0a3fa0
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-08-20 08:46:47 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens=[num for num in num_list if num%2==0]
+    print(evens)
+    return evens
+return_evens([0,1,3,5,7,8,9])
 
 def make_exclamation(sentence_list):
-    pass
+    sentence=[sentence +"!" for sentence in sentence_list]
+    print (sentence)
+    return sentence
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
